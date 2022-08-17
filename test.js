27f9083d73ddf7bace4ef16b014d865327cb23b8
Lamnet <semin0706@kaist.ac.kr>
2022-08-17 21:37:13 +0900

    [#0] Initial commit

---
+++ test.js
@@ -0,0 +1,15 @@
+var test = "Hell World <br/> HELL";
+console.log(test);
+
+var func_test = function(w, h)
+{
+    console.log("why this works?");
+}
+
+function func_test2()
+{
+    console.log("There are two types of function");
+}
+
+func_test();
+func_test2();
